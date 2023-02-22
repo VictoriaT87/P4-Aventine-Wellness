b08165de3a97fbfc27ea6a9d3f507fa66defe721
VictoriaT87 <victoriaemt@gmail.com>
2023-02-22 14:49:54 +0000

    Add models for appointments in models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Appointments(models.Model):
+    AVAILABLE_TIMES = (
+        ("9 AM", "9 AM"),
+        ("10:30 AM", "10:30 AM"),
+        ("12 PM", "12 PM"),
+        ("1:30 PM", "1:30 PM"),
+        ("3 PM", "3 PM"),
+        ("4:30 PM", "4:30 PM"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=10, choices=AVAILABLE_TIMES, default="9 AM")
