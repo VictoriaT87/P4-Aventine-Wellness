419db0c784af466099bfc0bc87a186671aa6c30c
VictoriaT87 <victoriaemt@gmail.com>
2023-03-14 14:44:13 +0000

    Install AllAuth, add basic Nav to base.html

--- aventine/urls.py
+++ aventine/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('booking.urls'), name="booking-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
