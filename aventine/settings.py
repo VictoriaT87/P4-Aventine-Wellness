69e68a6739d3e09ab8f58a811475c2a7caf60803
VictoriaT87 <victoriaemt@gmail.com>
2023-04-03 18:26:47 +0000

    Remove unwanted smtp address in settings.py

--- aventine/settings.py
+++ aventine/settings.py
@@ -214,14 +214,6 @@ ACCOUNT_FORMS = {
 }
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'aventinegardensp4@gmail.com'
-# EMAIL_HOST_PASSWORD = 'nvvhlxuzsnjnaxxk'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
