bf37455ed08c6e82ff5a6f14ae4813b6b2d93a2a
VictoriaT87 <victoriaemt@gmail.com>
2023-04-04 14:24:00 +0000

    Prepare for Heroku deployment

--- aventine/settings.py
+++ aventine/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["aventine-wellness-p4.herokuapp.com", "localhost"]
 
@@ -119,21 +119,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "aventine.wsgi.application"
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if "DATABASE_URL" in os.environ:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#         }
-#     }
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 # DATABASES = {
 #     'default': {
@@ -141,7 +143,7 @@ WSGI_APPLICATION = "aventine.wsgi.application"
 #     },
 # }
 
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 # Password validation
